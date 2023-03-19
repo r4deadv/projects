4a6696a6b8b0c79af9a3f6e4e296c4c70285e362
r4deadv <mihovilovic.rade@gmail.com>
2023-03-19 20:42:26 +0100

    Random CChoice Picker starter code added

--- features/double-vertical-slider/script.js
+++ features/double-vertical-slider/script.js
@@ -6,6 +6,7 @@ const downButton = document.querySelector(".down-button");
 const slidesLength = slideRight.querySelectorAll("div").length;
 
 let activeSlideIndex = 0;
+a;
 
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
