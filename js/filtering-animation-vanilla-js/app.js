74af4250d92b0a5be393b6c18e163add218a4bdc
r4deadv <mihovilovic.rade@gmail.com>
2024-02-12 15:47:44 +0100

    - adding a fading animation

--- js/filtering-animation-vanilla-js/app.js
+++ js/filtering-animation-vanilla-js/app.js
@@ -6,33 +6,31 @@ filterButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
     const filter = e.target.getAttribute('data-filter');
 
-    // change the active button
-    updateActiveButton(e.target);
-    // filter the list
-    filterConf(filter);
+    if (!document.startViewTransition) {
+      updateActiveButton(e.target);
+      filterConf(filter);
+    }
+
+    document.startViewTransition(() => {
+      updateActiveButton(e.target);
+      filterConf(filter);
+    });
   });
 });
 
 function updateActiveButton(newButton) {
-  // find the previously active button
-  // & remove the active class from it
   filterList.querySelector('.active').classList.remove('active');
 
-  // add the active class to our new button
   newButton.classList.add('active');
 }
 
 function filterConf(confFilter) {
-  // get each conference category
   conferences.forEach((conf) => {
     const confCategory = conf.getAttribute('data-category');
 
-    // check if that category matches the filter
     if (confFilter === 'all' || confFilter === confCategory) {
-      // if it matches, show that conf
       conf.removeAttribute('hidden');
     } else {
-      // if not, hide that conf
       conf.setAttribute('hidden', '');
     }
   });
