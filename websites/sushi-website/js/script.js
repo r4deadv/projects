c6cdddda8a3139e6c8ca1c679ffa07ce2f40fbff
r4deadv <mihovilovic.rade@gmail.com>
2025-03-12 22:35:44 +0100

    add AOS library and implement animations in trending section

--- websites/sushi-website/js/script.js
+++ websites/sushi-website/js/script.js
@@ -1,10 +1,13 @@
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 // init AOS animation
-AOS.init({
-    duration: 1000,
-    offset: 100,
+document.addEventListener("DOMContentLoaded", () => {
+    AOS.init({
+        duration: 1000,
+        offset: 100,
+    });
 });
+
 
 // the additional code you saw in the video will not be needed :)
