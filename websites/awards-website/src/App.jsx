dccd2d0108e3acf7b3329bb15e1a9855de77af32
r4deadv <mihovilovic.rade@gmail.com>
2025-03-16 23:33:36 +0100

    add Prettier and Tailwind CSS plugin, refactor App component, and create Hero component

--- websites/awards-website/src/App.jsx
+++ websites/awards-website/src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import Hero from './components/Hero.jsx'
 
 
-function App() {
+const App = () =>{
   return (
-    <main>
-      <h1 className='text-5xl text-violet-300 font-bold'>Welcome to Awwwards</h1>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'> 
+      <Hero />
     </main>
   )
 }
