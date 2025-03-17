fdaa92956a72f0b6e45a5225bb4d521dd1ac749a
r4deadv <mihovilovic.rade@gmail.com>
2025-03-17 12:26:50 +0100

    refactor Hero component to manage video state and enhance interactivity

--- websites/awards-website/src/App.jsx
+++ websites/awards-website/src/App.jsx
@@ -1,12 +1,11 @@
-import Hero from './components/Hero.jsx'
+import Hero from "./components/Hero.jsx";
 
-
-const App = () =>{
+const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'> 
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- websites/awards-website/src/components/Hero.jsx
+++ websites/awards-website/src/components/Hero.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setisLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4;
+  const nextVideoRef = useRef(null);
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  const handleMiniVdClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
       <div
@@ -8,7 +27,22 @@ const Hero = () => {
         className="relative z-10 h-dvh w-screen overflow-x-hidden rounded-lg bg-blue-75"
       >
         <div>
-          <div className="mask-clip-path">Rade</div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div
+              onClick={handleMiniVdClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                src={getVideoSrc(currentIndex + 1)}
+                ref={nextVideoRef}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
