e2443ba24a89943f679f93b336ce21c6b700fc6c
r4deadv <mihovilovic.rade@gmail.com>
2025-03-19 10:38:35 +0100

    add Story component and integrate it into App; create RoundedCorners component for SVG filters

--- websites/awards-website/src/App.jsx
+++ websites/awards-website/src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Features from "./components/Features.jsx";
+import Story from "./components/Story.jsx";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <About />
       <Features />
+      <Story />
     </main>
   );
 };
